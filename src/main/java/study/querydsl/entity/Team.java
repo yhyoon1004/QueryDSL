83bd22c0c84f52208a897fa374c2886f64478f53
yunhwan <yhyoon1004@naver.com>
2024-02-23 23:38:30 +0900

    예제 도메인 모델과 동작확인

---
+++ src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,27 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Team {
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+    @OneToMany(mappedBy = "team")
+    List<Member> members = new  ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+}
