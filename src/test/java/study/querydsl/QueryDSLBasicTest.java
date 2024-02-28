014721d2720a4f4433a99c6d1d61e74df51b4d83
yunhwan <yhyoon1004@naver.com>
2024-02-28 12:35:15 +0900

    집합

--- src/test/java/study/querydsl/QueryDSLBasicTest.java
+++ src/test/java/study/querydsl/QueryDSLBasicTest.java
@@ -1,6 +1,7 @@
 package study.querydsl;
 
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -11,12 +12,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
+import study.querydsl.entity.QTeam;
 import study.querydsl.entity.Team;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static study.querydsl.entity.QMember.member;// static import 로 해당 인스턴스를 가져다쓰는 방법
+import static study.querydsl.entity.QTeam.team;
 
 @SpringBootTest
 @Transactional
@@ -54,16 +57,16 @@ public class QueryDSLBasicTest {
     }
 
     @Test
-    public void startJPQL () throws Exception{
+    public void startJPQL() throws Exception {
         Member member = em.createQuery("select m from Member m where m.username = :username", Member.class)
                 .setParameter("username", "member1")
                 .getSingleResult();
         assertThat(member.getUsername()).isEqualTo("member1");
     }
-    
-    
+
+
     @Test
-    public void startQueryDSL () throws Exception{
+    public void startQueryDSL() throws Exception {
 //        QMember memberAAA = new QMember("memberAAA"); //생성자로 생성할 경우 엘리어스를 생성자의 문자열로 주게됨
         Member findMember = queryFactory
                 .select(member)
@@ -76,7 +79,7 @@ public class QueryDSLBasicTest {
 
 
     @Test
-    public void search() throws Exception{
+    public void search() throws Exception {
         Member findMember = queryFactory
                 .selectFrom(member)//이 메서드는 'select(member).from(member)'메서드를 합친 것
                 .where(member.username.eq("member1").and(member.age.eq(10)))
@@ -85,7 +88,7 @@ public class QueryDSLBasicTest {
     }
 
     @Test
-    public void searchAndParam() throws Exception{
+    public void searchAndParam() throws Exception {
         Member findMember = queryFactory
                 .selectFrom(member)
                 .where(member.username.eq("member1"),  //쉼표로 and() 메서드 대체 가능
@@ -95,7 +98,7 @@ public class QueryDSLBasicTest {
     }
 
     @Test
-    public void resultFetchTest () throws Exception{
+    public void resultFetchTest() throws Exception {
         //fetch() -> 결과가 없으면 빈 List 반환, fetchOne() -> 결과가 없으면 null 반환, 2개 이상이면 예외발생
 //        List<Member> fetch =  queryFactory.selectFrom(member).fetch();
 //
@@ -116,7 +119,7 @@ public class QueryDSLBasicTest {
      * 단 2 에서 회원 이름이 없으면 마지막에 출력  ->> nullsLast()
      */
     @Test
-    public void sortTest () throws Exception{
+    public void sortTest() throws Exception {
         em.persist(new Member(null, 100));
         em.persist(new Member("member10", 100));
         em.persist(new Member("member11", 100));
@@ -137,7 +140,7 @@ public class QueryDSLBasicTest {
     }
 
     @Test
-    public void pagingTest () throws Exception{
+    public void pagingTest() throws Exception {
         List<Member> result = queryFactory
                 .selectFrom(member)
                 .orderBy(member.username.desc())
@@ -148,7 +151,7 @@ public class QueryDSLBasicTest {
     }
 
     @Test
-    public void pagingTotalTest () throws Exception{
+    public void pagingTotalTest() throws Exception {
         QueryResults<Member> queryResults = queryFactory
                 .selectFrom(member)
                 .orderBy(member.username.desc())
@@ -157,8 +160,52 @@ public class QueryDSLBasicTest {
                 .fetchResults();
         assertThat(queryResults.getTotal()).isEqualTo(4);
         assertThat(queryResults.getLimit()).isEqualTo(2);
-        assertThat(queryResults.getOffset()).isEqualTo(1 );
+        assertThat(queryResults.getOffset()).isEqualTo(1);
         assertThat(queryResults.getResults().size()).isEqualTo(2);
+    }
+
+    @Test
+    public void aggregation() throws Exception {
+        List<Tuple> result = queryFactory.select(
+                        member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min()
+                )
+                .from(member)
+                .fetch();
+
+        Tuple tuple = result.get(0);
+
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+        assertThat(tuple.get(member.age.max())).isEqualTo(40);
+        assertThat(tuple.get(member.age.min())).isEqualTo(10);
+    }
+
+
+    /**
+     * 팀의 이름과 각 팀의 평균 연령을 구해라.
+     * */
+    @Test
+    public void groupByTest () throws Exception{
+        List<Tuple> result = queryFactory
+                .select(
+                        team.name,
+                        member.age.avg()
+                ).from(member).join(member.team, team)
+                .groupBy(team.name)
+                .fetch();
+        Tuple teamA = result.get(0);
+        Tuple teamB = result.get(1);
+
+        assertThat(teamA.get(team.name)).isEqualTo("teamA");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+
     }
 
 }
