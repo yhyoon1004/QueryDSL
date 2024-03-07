dec64df931ba2d6e7f4f196d62fc77eaaae71c20
yunhwan <yhyoon1004@naver.com>
2024-03-07 21:01:33 +0900

    프로젝션 결과 반환 - 기본

--- src/test/java/study/querydsl/QueryDSLBasicTest.java
+++ src/test/java/study/querydsl/QueryDSLBasicTest.java
@@ -445,5 +445,20 @@ public class QueryDSLBasicTest {
             System.out.println("item = " + item);
         }
     }
+    
+    @Test
+    public void tupleProjectionTest () throws Exception{
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            System.out.println("username = " + username);
+            System.out.println("age = " + age); 
+        }
+    }
 
 }
