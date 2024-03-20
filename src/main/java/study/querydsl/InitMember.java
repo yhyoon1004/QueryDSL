13ea123fd4afc38d061691cd41d2a9270a0b4deb
yunhwan <yhyoon1004@naver.com>
2024-03-20 17:21:03 +0900

    조회 API 컨트롤러 개발, 테스트 데이터 + 프로파일 분기

---
+++ src/main/java/study/querydsl/InitMember.java
@@ -0,0 +1,46 @@
+package study.querydsl;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
+
+@Profile("local")
+@Component
+@RequiredArgsConstructor
+public class InitMember {
+
+    private final InitMemberService initMemberService;
+
+    @PostConstruct
+    public void init() {
+        initMemberService.init();
+    }
+
+    @Component
+    static class InitMemberService {
+        @PersistenceContext
+        private EntityManager em;
+        @Transactional
+        public void init() {    //스프링 라이프 사이클이 있어서 위의 init() 메서드에 못넣어줌 @PostConstruct 과 Transaction
+            Team teamA = new Team("teamA");
+            Team teamB = new Team("teamB");
+            em.persist(teamA);
+            em.persist(teamB);
+
+            for (int i = 0; i < 100; i++) {
+                Team selectedTeam = i % 2 == 0 ? teamA : teamB;
+                em.persist(new Member("member" + i, i, selectedTeam));
+            }
+
+        }
+    }
+
+
+}
