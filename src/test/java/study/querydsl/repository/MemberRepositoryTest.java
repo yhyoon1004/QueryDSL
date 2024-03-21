9d22065fca1d839a676c2d76e6c50a6ce18a5a8d
yunhwan <yhyoon1004@naver.com>
2024-03-21 21:19:54 +0900

    스프링 데이터 JPA 레포지토리로 변경

---
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package study.querydsl.repository;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @Test
+    public void basicTest() {
+        Member member = new Member("member1", 10);
+        memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(member.getId()).get();
+        assertThat(findMember).isEqualTo(member);
+
+        List<Member> findAllResult = memberRepository.findAll();
+        assertThat(findAllResult).containsExactly(member);
+
+        List<Member> findByUsernameResult = memberRepository.findByUsername("member1");
+        assertThat(findByUsernameResult).containsExactly(member);
+
+    }
+}
