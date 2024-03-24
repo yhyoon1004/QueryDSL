fcec68d9ef37703cb75e8f3c1e96e4684c43a3b4
yunhwan <yhyoon1004@naver.com>
2024-03-24 13:28:19 +0900

    스프링 데이터 페이징 활용2 - countQuery 최적화

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -2,15 +2,19 @@ package study.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.support.PageableUtils;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.util.StringUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDTO;
 import study.querydsl.dto.QMemberTeamDTO;
+import study.querydsl.entity.Member;
 
 import java.util.List;
 
@@ -103,7 +107,8 @@ public class MemberRepositoryImpl implements MemberCustomRepository {
                 .limit(pageable.getPageSize())
                 .fetch();
 
-        long total = queryFactory.select(member)
+        JPAQuery<Member> countQuery = queryFactory
+                .select(member)
                 .from(member)
                 .leftJoin(member.team, team)
                 .where(
@@ -111,9 +116,9 @@ public class MemberRepositoryImpl implements MemberCustomRepository {
                         teamNameEq(condition.getTeamName()),
                         ageGOE(condition.getAgeGoe()),
                         ageLOE(condition.getAgeLoe())
-                )
-                .fetchCount();
-        return new PageImpl<>(content, pageable, total);
+                );
+//        return new PageImpl<>(content, pageable, total);
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount); //spring jpa 가 제공해주는 최적화 함수 -> 페이지 카운트 최적화 로직
     }
 
     private BooleanExpression usernameEq(String username) {
